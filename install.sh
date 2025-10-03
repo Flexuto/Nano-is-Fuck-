eabbec5d44e9c7eb96cad02d41b683f8e75b7004
Flexuto <166990749+Flexuto@users.noreply.github.com>
2025-10-03 21:31:35 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+echo '--Nano is Fuck!--'
+echo 'Remove Nano'
+pkg uninstall nano
+
+
+clear
+echo '--Nano is Fuck!(jokes lol)--'
+echo 'Download micro'
+pkg install micro
+
+clear
+echo 'Downloaded!'
+
